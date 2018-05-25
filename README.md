5d95744532726cd9653d1085ccd80a9a987081b5
Latamen <ait.latamene@live.fr>
2018-05-25 12:27:36 +0200

    1er commit - Ajout du tableau du jeu

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Projet 2048 développé avec VueJS
+# 2048
+
+> 2048 Project développé avec VueJS
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/Leaderboard.vue
@@ -0,0 +1,42 @@
+
+<template>
+    <table class="board" border="1"> 
+        <caption>Jeu 2048 ! :)</caption>
+        <tr v-for="n in this.numberOfLines"> 
+            <th></th> 
+            <th></th> 
+            <th></th> 
+            <th></th> 
+        </tr> 
+    </table>   
+</template>
+
+<script>
+import Board from "@/utils/Board"
+
+export default {
+  name: 'board',
+  components: {},
+  computed: {},
+  methods: {
+  },
+  data (){
+      return {
+          grid: [],
+          numberOfLines : 4
+      }
+  
+  }
+}
+</script>
+
+<style>
+board{
+    margin-left: 25%;
+}
+
+th {
+    height: 50px;
+    width: 50px;
+}
+</style>
