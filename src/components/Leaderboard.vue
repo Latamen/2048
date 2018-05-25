ca9a2f7c3420ab8032d8f6ab7fa99a1b69cb356e
Latamen <ait.latamene@live.fr>
2018-05-25 13:53:09 +0200

    Modif CSS + JS

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -1,14 +1,17 @@
 
 <template>
-    <table class="board" border="1"> 
-        <caption>Jeu 2048 ! :)</caption>
-        <tr v-for="n in this.numberOfLines"> 
-            <th></th> 
-            <th></th> 
-            <th></th> 
-            <th></th> 
-        </tr> 
-    </table>   
+    <div class="board">
+         <p>Score : </p>
+        <table class="boardTable" border="1"> 
+            <caption>Jeu 2048 ! :)</caption>
+            <tr v-for="n in this.numberOfLines"> 
+                <th></th> 
+                <th></th> 
+                <th></th> 
+                <th></th> 
+            </tr> 
+        </table>   
+    </div>
 </template>
 
 <script>
@@ -28,10 +31,20 @@ export default {
   
   }
 }
+
+// Renvoie un nombre aléatoire entre un minimum et un maximum (inclus)
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+console.log('Position de la 1ere case --> ' + getRandomInt(1, 4) + ' - ' + getRandomInt(1, 4));
+// expected output: 0, 1 or 2
 </script>
 
-<style>
-board{
+<style scoped>
+.boardTable{
     margin-left: 25%;
 }
 
